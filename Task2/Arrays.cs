f0fa68ff1bd14d1bcc2b9638846a4a4540749620
Haritanagarajan <30harita2002@gmail.com>
2023-07-27 12:59:19 +0530

    task2 commit

--- Task2/Arrays.cs
+++ Task2/Arrays.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Task2
 {
     internal class Arrays
     {
         public string empname;
-        public int id;
-        public double salary;
-        public double bonus;
+        public int empid;
+        public double empsal;
+        public double empbonus;
+
         public static void D2arrays()
         {
             Console.WriteLine("2d ARRAYS");
@@ -66,7 +68,7 @@ namespace Task2
             //2 arrays 
             int[][,] jaggedarr = new int[3][,]
              {
-             new int[, ] { {1, 3}, {5, 7} },
+             new int[, ] { {1, 3}, {5, 7}, {8, 10}  },
              new int[, ] { {0, 2}, {4, 6}, {8, 10} },
              new int[, ] { {7, 8}, {3, 1}, {0, 6} },
              };
@@ -75,13 +77,19 @@ namespace Task2
 
             for (int i = 0; i < jaggedarr.Length; i++)
             {
-             for(int j= 0;j < jaggedarr[i].Length; j++)
+                //int rows = 0;
+
+                for (int j = 0; j<jaggedarr[i].GetLength(0); j++)
                 {
-                    Console.WriteLine(jaggedarr[i]);
+                    Console.WriteLine("\n");
+                    for(int k = 0; k < jaggedarr[j].GetLength(1);k++)
+                    {
+                        Console.WriteLine($"{jaggedarr[i][j, k]}" + " ");
+                        Console.Write("\t");
+                    }
                 }
             }
-
-        }
+           }
 
 
         public static void Traineedetails(params object[] elements)
@@ -96,50 +104,50 @@ namespace Task2
 
         }
 
-        public void Employees()
+        public void Employees(out double bonuscal)
         {
 
-            Console.WriteLine("Enter the empname:");
-            empname = Console.ReadLine();
+            bonuscal = 0.2 * empsal + empbonus;
+            Console.WriteLine($"THE BONUS IS:{bonuscal}");
 
-            Console.WriteLine("Enter the empid:");
-            id = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the salary:");
-            salary = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("Enter the bonus amnt:");
-            bonus = Convert.ToInt64(Console.ReadLine());
         }
 
-        public void calculatebonus()
-        {
-            double bonuscal;
-
-            bonuscal = 0.2 * salary;
-
-            Console.WriteLine($"Mr/Mrs {empname} bonus amount is {bonuscal}");
-
-            Console.WriteLine(bonuscal);
-        }
 
         public static void Main(string[] args)
-        { 
+        {
             Arrays arr = new Arrays();
             //objects for traineedetails mehod
             object[] t1 = { "Harita", 21, 6382830515, "kovaipudur" };
             object[] t2 = { "sharon", 21, 9984563765, "kovaipudur" };
             object[] t3 = { "Ranita", 21, 2351897265, "kovaipudur" };
             object[] t4 = { "Harishmitha", 21, 1237895643, "kovaipudur" };
-            D2arrays();
+            //D2arrays();
             Jaggedarrays();
             //usingparams concept
-            Traineedetails(t1);
-            Traineedetails(t2);
-            Traineedetails(t3);
-            Traineedetails(t4);
-            arr.Employees();
-            arr.calculatebonus();
+            //Traineedetails(t1);
+            //Traineedetails(t2);
+            //Traineedetails(t3);
+            //Traineedetails(t4);
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter the empname:");
+                arr.empname = Console.ReadLine();
+
+                Console.WriteLine("Enter the empid:");
+                arr.empid = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the salary:");
+                arr.empsal = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("Enter the bonus amnt:");
+                arr.empbonus = Convert.ToInt64(Console.ReadLine());
+
+                double bbonus;
+                arr.Employees(out bbonus);
+
+            }
+
             Console.ReadLine();
 
         }
